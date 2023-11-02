908ef5d9e4ac3a4986af04c02776dee22e8c8d89
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-11-02 21:43:08 +0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,98 @@
+<div
+	class="d-flex flex-column"
+	style="height:100vh; background: url(https://webassets.tomtom.com/m/52bd3bb5a537fbba/original/slider_Daimler_1920x800.jpg) center no-repeat; background-size: cover;">
+	<nav class="bg-light bg-opacity-75 text-dark w-100" style="backdrop-filter: blur(20em)">
+		<div
+			class="d-flex justify-content-between align-items-center flex-wrap px-5 py-4"
+			style="font-size:1.4em">
+			<div class="flex-grow-1">+7 (985) 388-74-47</div>
+			<div class="display-6 fw-bold">ООО "КЛЭВЭР ЛОГИСТИКС"</div>
+			<div class="flex-grow-1 text-end">Время работы: Пн-Пт <br /> с 9:00 до 18:00</div>
+		</div>
+	</nav>
+	<div class="flex-grow-1 d-flex align-items-center justify-content-start">
+		<div
+			class="bg-light bg-opacity-10 text-light rounded-1 p-5 m-5 w-50"
+			style="backdrop-filter: blur(.2em)">
+			<h1 class="display-4 fw-bold">МЫ - КЛЭВЭР ЛОГИСТИКС</h1>
+			<hr class="bg-light" style="height:2px" />
+			<h2 class="text-light text-opacity-75">
+				Логистическая компания, осуществляющая полный комплекс транспортно-экспедиторских услуг по
+				международной доставке грузов.
+			</h2>
+			<div>
+				<a
+					href="#about"
+					class="btn btn-light bg-light bg-opacity-25 text-light border-0 mt-3 rounded-1"
+					>Кто мы?</a>
+				<a
+					href="#work"
+					class="btn btn-light bg-light bg-opacity-25 text-light border-0 mt-3 rounded-1"
+					>Чем мы занимаемся?</a>
+				<a
+					href="#map"
+					class="btn btn-light bg-light bg-opacity-25 text-light border-0 mt-3 rounded-1"
+					>Где мы находимся?</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="about" class="bg-light p-5" style="height:100vh; font-size: 1.5em;">
+	<div
+		class="d-flex flex-column justify-content-start align-items-top p-5 gap-0 bg-success-subtle bg-opacity-10 rounded-1"
+		style="backdrop-filter: blur(2em)">
+		<div>
+			<b>Г</b>оловной офис нашей компании располагается в городе Дубна.
+		</div>
+		<hr />
+		<div>
+			<b>Г</b>еография услуг компании охватывает страны Западной и Центральной Европы, Российскую
+			Федерацию, Беларусь, Украину, Молдову, Казахстан, Узбекистан и другие страны СНГ.
+		</div>
+		<hr />
+		<div>
+			<b>Н</b>аш коллектив – хорошо организованная команда высококвалифицированных специалистов в
+			области транспорта и логистики. Клиенты КЛЭВЭР ЛОГИСТИКС – это крупные международные концерны,
+			средние и малые предприятия России и стран Европейского Союза.
+		</div>
+		<hr />
+		<div>
+			<b>М</b>ы гарантируем своим клиентам четкое выполнение условий договоров, стабильность,
+			уважение и в обязательном порядке высокое качество.
+		</div>
+		<hr />
+		<div>
+			<b>К</b>омпания КЛЭВЭР ЛОГИСТИКС осуществляет международные автоперевозки с использованием
+			наиболее оптимальных логистических схем.
+		</div>
+	</div>
+</div>
+
+<div id="work" class="d-flex flex-column gap-3 p-5 m-5 text-light" style="min-height:100vh">
+	<div class="d-flex gap-3">
+		<div
+			class="d-flex align-items-center justify-content-center w-50 rounded-1"
+			style="height:15em; background: url(https://cleverlogistics.ru/images/good_block_1.jpg) center no-repeat; background-size: cover;">
+			<b>ПЕРЕВОЗКИ КОМПЛЕКТНЫХ ГРУЗОВ</b>
+		</div>
+		<div
+			class="d-flex align-items-center justify-content-center w-50 rounded-1"
+			style="height:15em; background: url(https://cleverlogistics.ru/images/good_block_2.jpg) center no-repeat; background-size: cover;">
+			<b>ПЕРЕВОЗКИ СБОРНЫХ ГРУЗОВ</b>
+		</div>
+	</div>
+
+	<div class="d-flex gap-3">
+		<div
+			class="d-flex align-items-center justify-content-center w-50 rounded-1"
+			style="height:15em; background: url(https://cleverlogistics.ru/images/good_block_3.jpg) center no-repeat; background-size: cover;">
+			<b>ПЕРЕВАЛКА И СКЛАДСКОЕ ХРАНЕНИЕ, КОНСОЛИДАЦИЯ ГРУЗОВ</b>
+		</div>
+		<div
+			class="d-flex align-items-center justify-content-center w-25 rounded-1"
+			style="height:15em; background: url(https://cleverlogistics.ru/images/good_block_3.jpg) center no-repeat; background-size: cover;">
+			<b>ПЕРЕВАЛКА И СКЛАДСКОЕ ХРАНЕНИЕ, КОНСОЛИДАЦИЯ ГРУЗОВ</b>
+		</div>
+	</div>
+</div>
---
+++ src/routes/old/+page.svelte
@@ -0,0 +1,38 @@
+<nav class="sticky-top">
+	<div class="container px-5">
+		<div class="d-flex align-items-center justify-content-between flex-wrap py-3">
+			<div class="my-text-primary" style="font-family: 'ClearSans-bold'; font-size:30px;">
+				ООО "КЛЭВЭР ЛОГИСТИКС"
+			</div>
+			<div>Время работы: Пн-Пт, с 9:00 до 18:00</div>
+			<div style="font-family: 'ClearSans-bold'; font-size: 24px;">+7 (985) 388-74-47</div>
+		</div>
+	</div>
+</nav>
+<div
+	style="height: 22em; background: url(https://cleverlogistics.ru/images/block_1_bg.jpg) center no-repeat; backdrop-size: cover">
+	<div class="container px-5 h-100 text-light">xbx</div>
+</div>
+
+<style>
+	@font-face {
+		font-family: 'ClearSans';
+		src: url(/ClearSans/ClearSans.eot) format('eot');
+	}
+	@font-face {
+		font-family: 'ClearSans-bold';
+		src: url(/ClearSans/ClearSans-Bold.eot) format('eot');
+	}
+	:root {
+		--primary-color: #26a1da;
+	}
+
+	body {
+		font-size: 16px;
+		font-family: 'ClearSans';
+	}
+
+	.my-text-primary {
+		color: var(--primary-color);
+	}
+</style>
